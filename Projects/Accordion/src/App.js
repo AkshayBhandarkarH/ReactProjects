67cf8f67cb9cac95389b738f14d46d717b6c57d2
akshaybhandarkarh <akshaybhandarkar654@gmail.com>
2024-06-07 12:47:40 +0530

    Added Star-Rating Component

--- Projects/Accordion/src/App.js
+++ Projects/Accordion/src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import Accordion from "./Components/myaccordion";
 import "./Components/myaccordion/styles.css";
 import ColorPicker from "./Components/ColorPicker";
+import StarRating from "./Components/starRating/StarRating";
 function App() {
   return (
     <div className="App">
       <Accordion />
       <ColorPicker />
+      <StarRating />
     </div>
   );
 }
